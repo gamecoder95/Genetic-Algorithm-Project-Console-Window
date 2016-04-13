ecd09a1f52428d86efd01033d155f8039ec39f00
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2016-04-13 09:49:17 -0400

    Changed a few things in main as well.

--- COEN 244 Project/main.cpp
+++ COEN 244 Project/main.cpp
@@ -1,35 +1,52 @@
 #include "Controller.h"
 #include "LangermannPoint.h"
+#include <limits>// For "numeric_limits" used to ensure valid user input for initial population.
 using namespace std;
 
 int main()
 {
-    // 'O' for optimization, 'C' for curve fitting.
+    // Display title.
     cout<<"Genetic Algorithm!"<<endl;
     cout<<"\tBy Ahmad El-Baher"<<endl;
     cout<<"\tDate: 12/4/16"<<endl;
     cout<<"\tSID: 40000968"<<endl;
     char choice = 'O';
 
+
+    // Display menu.
     cout<<endl<<endl<<"Choose an application to view."<<endl;
     cout<<"O - Optimization"<<endl;
     cout<<"C - Curve Fitting"<<endl;
     cout<<"Enter the character corresponding to your choice (default = O): ";
     cin>>choice;
 
+    // Set the problem type (application) depending on the user input.
     Controller::setProblemType((choice == 'C' || choice == 'c')? Controller::CURVE_FITTING : Controller::OPTIMIZATION);
 
+    // Request initial population size.
     int initPopSize = 0;
-    cout<<endl<<endl<<"Enter an initial population size (default = "<<Controller::DEFAULT_POP_SIZE<<"): ";
+    cout<<endl<<endl<<"Enter an initial population size (minimum = "<<Controller::DEFAULT_POP_SIZE<<"): ";
     cin>>initPopSize;
 
+    // To ensure that the user enters only integers greater than 100.
+    while(cin.fail() || initPopSize < Controller::DEFAULT_POP_SIZE)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout<<"Invalid input. Please enter an integer greater than "<<Controller::DEFAULT_POP_SIZE<<": ";
+        cin>>initPopSize;
+    }
+
+    // Set the initial population size.
     Controller::setInitPopSize(initPopSize);
 
+    // Display result of choices, and allowing user to view them before beginning at his/her will.
     cout<<endl<<endl<<"You have chosen to view "<<((Controller::getProblemType() == Controller::OPTIMIZATION)? "Optimization" : "Curve Fitting");
     cout<<" with an initial population size of "<<Controller::getInitPopSize()<<endl;
     cout<<"Enter a non-space character to begin: ";
-    cin>>choice; // Re-using choice to store the character to enter to begin the program.
+    cin>>choice; // Re-using choice to store the character to begin the program.
 
+    // The entire algorithm is implemented in Controller, and is executed through this function.
     Controller::run();
 
     return 0;
